06f347d055fb6f2362cc1359b6bc9cab05c9cd69
abdinegara783 <abdinegara783@gmail.com>
2024-12-24 19:46:20 +0700

    report Page

--- base/admin.py
+++ base/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from .models import Question, Response, SubQuestion, Section
+
 # Register your models here.
+
+# from .models import Profile
+
+admin.site.register(Question)
+admin.site.register(Response)
+admin.site.register(SubQuestion)
+admin.site.register(Section)
